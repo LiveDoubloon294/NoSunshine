222d9922eec1252699f6d54f7c798a64d6c195dd
LiveDoubloon294 <134166655+LiveDoubloon294@users.noreply.github.com>
2026-09-23 11:45:11 -0700

    Add files via upload

---
+++ NoSunshine.java
@@ -0,0 +1,70 @@
+public class NoSunshine{
+   public static void main(String[] args){
+      a();
+      g("It's not warm when she's away");
+      a();
+      g("And she's always gone too long");
+      b();
+      
+      c();
+      
+      g("Wonder this time where she's gone");
+      g("Wonder if she's gone to stay");
+      a();
+      e();
+      b();
+      
+      c();
+      
+      System.out.print("And ");
+      f(3);
+      for (int g = 0; g < 4; g++){
+         f(4);
+      }
+      f(1);
+      
+      c();
+      
+      g("Hey, I ought to leave young things alone\nBut ain't no sunshine when she's gone, whoa-whoa");
+      a();
+      g("Only darkness every day");
+      
+      c();
+      
+      a();
+      e();
+      
+      c();
+      
+      for(int i = 0; i < 4; i++){
+         b();
+      }
+   }  
+   
+   public static void a(){
+      g("Ain't no sunshine when she's gone");
+   }
+   public static void b(){
+      g("Anytime she goes away");
+   }
+   public static void c(){
+      System.out.print("\n");
+   }
+   public static void d(){
+      System.out.print("I know");
+   }
+   public static void e(){
+      g("And this house just ain't no home");
+   }
+   public static void f(int loopConstraint){
+      for (int i = 0; i < loopConstraint; i++){
+            d();
+            System.out.print(", ");
+         }
+         d();
+         c();
+   }
+   public static void g(String txt){
+      System.out.println(txt);
+   }
+}
